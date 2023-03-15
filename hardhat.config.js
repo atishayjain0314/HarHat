d420dd3df6a4e610afdadccca617050732cb65f5
200110022atishay <atishayjain0314@gmail.com>
2023-03-15 20:16:54 +0530

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle")
+const ALCHEMY_API_KEY = "upFcjVsfwhVOhJsQ5VB7Wkt-Klf2fsOz";
+const MUMBAI_PRIVATE_KEY = "";
 module.exports = {
   solidity: "0.8.18",
+  networks:{
+    mumbai:{
+      url:`https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts:[`${MUMBAI_PRIVATE_KEY}`]
+    }
+  }
 };
